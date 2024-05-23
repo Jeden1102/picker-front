fa6aa03beee09cb8b170b8498dcd0e285da806d2
jeden1102 <bum123@o2.pl>
2024-05-23 20:32:40 +0200

    master. Implemented middleware for auth + anonymous routes.

---
+++ middleware.ts
@@ -0,0 +1,22 @@
+import { NextResponse } from "next/server";
+import type { NextRequest } from "next/server";
+
+export function middleware(request: NextRequest) {
+  const cookie = request.cookies.get("jwt");
+
+  const currentPath = request.nextUrl.pathname;
+
+  const authPaths = ["/user"];
+
+  const anonymousPaths = ["/login", "/register"];
+
+  if (authPaths.includes(currentPath) && cookie === undefined) {
+    return NextResponse.redirect(new URL("/login", request.url));
+  }
+
+  if (anonymousPaths.includes(currentPath) && cookie) {
+    return NextResponse.redirect(new URL("/user", request.url));
+  }
+
+  return NextResponse.next();
+}
