a453cdb036e6f9f378959d0009cc344586ec9c19
jeden1102 <bum123@o2.pl>
2024-06-10 16:27:53 +0200

    master. Scraping help tabs applied activeStep number.

--- components/scrape/scrape-side-tab-help.tsx
+++ components/scrape/scrape-side-tab-help.tsx
@@ -1,75 +1,71 @@
 import { Button } from "@nextui-org/button";
 import {
-    Modal,
-    ModalContent,
-    ModalHeader,
-    useDisclosure,
-    ModalBody,
-    ModalFooter,
+  Modal,
+  ModalContent,
+  ModalHeader,
+  useDisclosure,
+  ModalBody,
+  ModalFooter,
 } from "@nextui-org/modal";
 import { TfiHelpAlt } from "react-icons/tfi";
 
+interface Props {
+  activeStep: number;
+}
 
-function ScrapeSideTabHelp() {
-    const { isOpen, onOpen, onOpenChange } = useDisclosure();
-    return (
-        <>
-            <Button onPress={onOpen}><TfiHelpAlt /></Button>
-            <Modal
-                className="absolute right-0 top-0 h-full m-0 sm:m-0 md:rounded-r-none"
-                backdrop="opaque"
-                isOpen={isOpen}
-                onOpenChange={onOpenChange}
-            >
-                <ModalContent>
-                    {(onClose) => (
-                        <>
-                            <ModalHeader className="flex flex-col gap-1">
-                                Scraping proccess help
-                            </ModalHeader>
-                            <ModalBody>
-                                <p>
-                                    Lorem ipsum dolor sit amet, consectetur
-                                    adipiscing elit. Nullam pulvinar risus non
-                                    risus hendrerit venenatis. Pellentesque sit
-                                    amet hendrerit risus, sed porttitor quam.
-                                </p>
-                                <p>
-                                    Lorem ipsum dolor sit amet, consectetur
-                                    adipiscing elit. Nullam pulvinar risus non
-                                    risus hendrerit venenatis. Pellentesque sit
-                                    amet hendrerit risus, sed porttitor quam.
-                                </p>
-                                <p>
-                                    Magna exercitation reprehenderit magna aute
-                                    tempor cupidatat consequat elit dolor
-                                    adipisicing. Mollit dolor eiusmod sunt ex
-                                    incididunt cillum quis. Velit duis sit
-                                    officia eiusmod Lorem aliqua enim laboris do
-                                    dolor eiusmod. Et mollit incididunt nisi
-                                    consectetur esse laborum eiusmod pariatur
-                                    proident Lorem eiusmod et. Culpa deserunt
-                                    nostrud ad veniam.
-                                </p>
-                            </ModalBody>
-                            <ModalFooter>
-                                <Button
-                                    color="danger"
-                                    variant="light"
-                                    onPress={onClose}
-                                >
-                                    Close
-                                </Button>
-                                <Button color="primary" onPress={onClose}>
-                                    Action
-                                </Button>
-                            </ModalFooter>
-                        </>
-                    )}
-                </ModalContent>
-            </Modal>
-        </>
-    );
+function ScrapeSideTabHelp({ activeStep }: Props) {
+  const { isOpen, onOpen, onOpenChange } = useDisclosure();
+  return (
+    <>
+      <Button onPress={onOpen}>
+        <TfiHelpAlt />
+      </Button>
+      <Modal
+        className="absolute right-0 top-0 h-full m-0 sm:m-0 md:rounded-r-none"
+        backdrop="opaque"
+        isOpen={isOpen}
+        onOpenChange={onOpenChange}
+      >
+        <ModalContent>
+          {(onClose) => (
+            <>
+              <ModalHeader className="flex flex-col gap-1">
+                Scraping proccess help {activeStep}
+              </ModalHeader>
+              <ModalBody>
+                <p>
+                  Lorem ipsum dolor sit amet, consectetur adipiscing elit.
+                  Nullam pulvinar risus non risus hendrerit venenatis.
+                  Pellentesque sit amet hendrerit risus, sed porttitor quam.
+                </p>
+                <p>
+                  Lorem ipsum dolor sit amet, consectetur adipiscing elit.
+                  Nullam pulvinar risus non risus hendrerit venenatis.
+                  Pellentesque sit amet hendrerit risus, sed porttitor quam.
+                </p>
+                <p>
+                  Magna exercitation reprehenderit magna aute tempor cupidatat
+                  consequat elit dolor adipisicing. Mollit dolor eiusmod sunt ex
+                  incididunt cillum quis. Velit duis sit officia eiusmod Lorem
+                  aliqua enim laboris do dolor eiusmod. Et mollit incididunt
+                  nisi consectetur esse laborum eiusmod pariatur proident Lorem
+                  eiusmod et. Culpa deserunt nostrud ad veniam.
+                </p>
+              </ModalBody>
+              <ModalFooter>
+                <Button color="danger" variant="light" onPress={onClose}>
+                  Close
+                </Button>
+                <Button color="primary" onPress={onClose}>
+                  Action
+                </Button>
+              </ModalFooter>
+            </>
+          )}
+        </ModalContent>
+      </Modal>
+    </>
+  );
 }
 
 export default ScrapeSideTabHelp;
--- components/scrape/scrape-side-tabs.tsx
+++ components/scrape/scrape-side-tabs.tsx
@@ -1,15 +1,20 @@
 import ScrapeSideTabHelp from "./scrape-side-tab-help";
 import ScrapeSideTabResults from "./scrape-side-tab-results";
 import { Card, CardBody } from "@nextui-org/card";
-function ScrapeSideTabs() {
-    return (
-        <Card className="fixed right-0 bottom-4 xl:bottom-1/2 z-50 flex flex-col gap-4 md:rounded-r-none">
-            <CardBody className="flex flex-col gap-4">
-                <ScrapeSideTabHelp />
-                <ScrapeSideTabResults />
-            </CardBody>
-        </Card>
-    );
+
+interface Props {
+  activeStep: number;
+}
+
+function ScrapeSideTabs({ activeStep }: Props) {
+  return (
+    <Card className="fixed right-0 bottom-4 xl:bottom-1/2 z-50 flex flex-col gap-4 md:rounded-r-none">
+      <CardBody className="flex flex-col gap-4">
+        <ScrapeSideTabHelp activeStep={activeStep} />
+        <ScrapeSideTabResults />
+      </CardBody>
+    </Card>
+  );
 }
 
 export default ScrapeSideTabs;
