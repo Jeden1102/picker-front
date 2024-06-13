0b9c08a8c5538279ef1b8fa5d0c5fe53745926d2
jeden1102 <bum123@o2.pl>
2024-06-13 21:22:31 +0200

    master. Terms page populated with api data.

--- app/(pages)/terms/layout.tsx
+++ app/(pages)/terms/layout.tsx
@@ -1,3 +1,5 @@
+import "@/styles/terms.css";
+
 export default function AboutLayout({
   children,
 }: {
@@ -5,9 +7,7 @@ export default function AboutLayout({
 }) {
   return (
     <section className="flex flex-col items-center justify-center gap-4 pb-2 md:py-10">
-      <div className="inline-block max-w-lg text-center justify-center">
-        {children}
-      </div>
+      <div className="inline-block justify-center">{children}</div>
     </section>
   );
 }
--- app/(pages)/terms/page.tsx
+++ app/(pages)/terms/page.tsx
@@ -1,11 +1,24 @@
-"use client";
-
 import { title } from "@/components/primitives";
 
-export default function TermsPage() {
+async function getData() {
+  const res = await fetch("http://localhost:1337/api/terms");
+
+  if (!res.ok) {
+    throw new Error("Failed to fetch data");
+  }
+
+  return res.json();
+}
+
+export default async function TermsPage() {
+  const data = await getData();
   return (
     <div>
       <h1 className={title({ size: "md" })}>Terms</h1>
+      <div
+        className="mt-4 terms"
+        dangerouslySetInnerHTML={{ __html: data.data[0].attributes.content }}
+      ></div>
     </div>
   );
 }
