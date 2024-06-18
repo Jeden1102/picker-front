ab2e569b4f9ceb365c395b85ad9c13f991409c53
jeden1102 <bum123@o2.pl>
2024-06-18 22:25:22 +0200

    master. Scraping steps help stat.

--- components/scrape/scrape-side-tab-help.tsx
+++ components/scrape/scrape-side-tab-help.tsx
@@ -15,6 +15,13 @@ interface Props {
 
 function ScrapeSideTabHelp({ activeStep }: Props) {
   const { isOpen, onOpen, onOpenChange } = useDisclosure();
+
+  const headings = [
+    "General info",
+    "Authorization",
+    "Scrape details",
+    "Summary",
+  ];
   return (
     <>
       <Button onPress={onOpen}>
@@ -30,27 +37,59 @@ function ScrapeSideTabHelp({ activeStep }: Props) {
           {(onClose) => (
             <>
               <ModalHeader className="flex flex-col gap-1">
-                Scraping proccess help {activeStep}
+                Scraping proccess help - {headings[activeStep]}
               </ModalHeader>
               <ModalBody>
-                <p>
-                  Lorem ipsum dolor sit amet, consectetur adipiscing elit.
-                  Nullam pulvinar risus non risus hendrerit venenatis.
-                  Pellentesque sit amet hendrerit risus, sed porttitor quam.
-                </p>
-                <p>
-                  Lorem ipsum dolor sit amet, consectetur adipiscing elit.
-                  Nullam pulvinar risus non risus hendrerit venenatis.
-                  Pellentesque sit amet hendrerit risus, sed porttitor quam.
-                </p>
-                <p>
-                  Magna exercitation reprehenderit magna aute tempor cupidatat
-                  consequat elit dolor adipisicing. Mollit dolor eiusmod sunt ex
-                  incididunt cillum quis. Velit duis sit officia eiusmod Lorem
-                  aliqua enim laboris do dolor eiusmod. Et mollit incididunt
-                  nisi consectetur esse laborum eiusmod pariatur proident Lorem
-                  eiusmod et. Culpa deserunt nostrud ad veniam.
-                </p>
+                {activeStep === 0 && (
+                  <>
+                    <p>Please fill all fields to proceed to the next step.</p>
+                    <p>
+                      <b className="pr-1">Scrape title</b>
+                      will be a field corresponding to the name of scraping
+                      you've applied. Make it stand out!
+                    </p>
+                    <p>
+                      <b className="pr-1">Scrape description</b>
+                      will be a field corresponding to the description of your
+                      scrape. Generally just let the users know what is your
+                      scraped use for. You can provide some use cases etc.
+                    </p>
+                    <p>
+                      <b className="pr-1">Website</b>
+                      will be a field corresponding to the website of scrape
+                      target. Pass only the part without the protocol (https) -
+                      we've got it for you!
+                    </p>
+                    <p>
+                      If you are ready with the fields. Proceed to the next step
+                      ;)
+                    </p>
+                  </>
+                )}
+                {activeStep === 1 && (
+                  <>
+                    <p>
+                      <b className="pr-1">Scraping available public</b>- select
+                      this option if you want to publish your scrape to all
+                      users. It will be visible public.
+                    </p>
+                    <p>
+                      <b className="pr-1">Scraper endpoints public available</b>
+                      - select this option if you want to make all your
+                      endpoints available without any authorization. Even if
+                      your scrape is not public available, by selecting this
+                      option, the endpoints are accessible without any key.
+                    </p>
+                    <p>
+                      <b className="pr-1">Authorization key</b>
+                      key needed for api endpoint authorization.
+                    </p>
+                    <p>
+                      If you are ready with the fields. Proceed to the next step
+                      ;)
+                    </p>
+                  </>
+                )}
               </ModalBody>
               <ModalFooter>
                 <Button color="danger" variant="light" onPress={onClose}>
